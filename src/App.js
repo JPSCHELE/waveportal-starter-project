815b916b1f55fc497fe0dcaa783fed57d2cfa8e2
Juan Pablo Schele <juan@archfinance.io>
2022-05-22 17:12:48 -0400

    ✨ Connect metamask

--- src/App.js
+++ src/App.js
@@ -1,11 +1,62 @@
-import * as React from "react";
-import { ethers } from "ethers";
+import React, {useEffect, useState} from "react";
+// import { ethers } from "ethers";
 import './App.css';
 
 export default function App() {
+  const [currentAccount, setCurrentAccount] = useState("");
+  
+  const checkIfWalletIsConnected = async () => {
+    try{
+      const { ethereum } = window;
+    
+    if (!ethereum){
+      console.log("Wallet not connected")
+    } else {
+      console.log("Wallet connected", ethereum)
+    }
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account:", account);
+      setCurrentAccount(account)
+    } else {
+      console.log("No authorized account found")
+    }
+  } catch (error){
+    console.log(error)
+  }
+  }
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  },[])
+
+   /*
+      * Check if we're authorized to access the user's wallet
+      */
+  
+
+
 
   const wave = () => {
-    
   }
   
   return (
@@ -28,6 +79,14 @@ export default function App() {
         <button className="waveButton" onClick={wave}>
           Wave at Me
         </button>
+
+
+        {!currentAccount && (
+          <button className="waveButton" onClick={connectWallet}>
+            Connect Wallet
+          </button>
+        )}
+
       </div>
     </div>
   );
